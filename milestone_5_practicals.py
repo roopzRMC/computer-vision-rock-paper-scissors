3b8733c91f62ca662de7d0975cb0b01da66c57ff
Rupert Coghlan <r.coghlan@gmail.com>
2022-11-20 20:21:01 +0000

    adding squared and cubed lambda funcs

---
+++ milestone_5_practicals.py
@@ -0,0 +1,19 @@
+# %%
+import os
+os.getcwd()
+# %%
+"""
+
+A lambda function that takes in a number and returns that number cubed
+A lambda function that takes two numbers and returns the sum of those numbers
+A lambda function that takes two numbers and returns the product of those numbers
+"""
+# lamdba function function that returns the square of an input
+squared_lambda = lambda x: x ** 2
+# %%
+squared_lambda(9)
+# %%
+cubed_lambda = lambda x: x ** 3
+# %%
+cubed_lambda(3)
+# %%
