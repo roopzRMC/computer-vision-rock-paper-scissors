7d29cd3ad20e6fb1db8af2ce17e4d7eafacc0157
Rupert Coghlan <r.coghlan@gmail.com>
2022-11-20 20:26:57 +0000

    adding simple lambda functions

--- milestone_5_practicals.py
+++ milestone_5_practicals.py
@@ -4,16 +4,28 @@ os.getcwd()
 # %%
 """
 
-A lambda function that takes in a number and returns that number cubed
-A lambda function that takes two numbers and returns the sum of those numbers
-A lambda function that takes two numbers and returns the product of those numbers
+
+
 """
 # lamdba function function that returns the square of an input
 squared_lambda = lambda x: x ** 2
 # %%
 squared_lambda(9)
 # %%
+# A lambda function that takes in a number and returns that number cubed
 cubed_lambda = lambda x: x ** 3
 # %%
 cubed_lambda(3)
+# %%
+# A lambda function that takes two numbers and returns the sum of those numbers
+
+sum_lambda = lambda x, y: x + y     
+# %%
+sum_lambda(5,3)
+# %%
+# A lambda function that takes two numbers and returns the product of those numbers
+
+product_lambda = lambda x, y: x * y
+# %%
+product_lambda(8,6)
 # %%
